78ec72c59730a5452f127cded5e63cafbc247753
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2022-12-03 23:21:55 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (57.62%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+         s=s.trim();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char ch=s.charAt(i);
+            if(ch!=' ')
+                len++;
+            else
+                break;
+        }
+        return len;
+    }
+}

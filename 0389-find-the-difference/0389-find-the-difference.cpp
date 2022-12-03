27b9b2e22a57a96ebdde9571b18b07991d44d022
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2022-12-03 23:30:57 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (25.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char c=0;
+        for(int i=0;i<s.size();i++)
+            c=c^s[i];
+        for(int i=0;i<t.size();i++)
+            c=c^t[i];
+        return c;
+    }
+};

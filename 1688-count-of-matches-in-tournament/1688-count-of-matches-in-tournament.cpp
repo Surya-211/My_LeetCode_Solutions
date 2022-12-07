122f0f52998ab40f477d51edd1f08c6d17eaa69f
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2022-12-08 01:27:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (23.66%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int count=0;
+        while(n!=1)
+        {
+            if(n%2==0)
+            {
+                count+=n/2;
+                n=n/2;
+            }
+            else
+            {
+                count+=n/2+1;
+                n=n/2;
+            }
+        }
+        return count;
+    }
+};

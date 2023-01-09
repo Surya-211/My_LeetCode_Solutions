dc0503a70f35af176adc40ba9e5f1e2379fe48ef
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-01-09 23:51:19 +0530

    Time: 3 ms (97.48%), Space: 7.1 MB (47.31%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<s.size();i++)
         {
             
-            if(m1[s[i]]==0&&m2[t[i]]==0)
+            if(m1.find(s[i])==m1.end()&&m2.find(t[i])==m2.end())
             {
                 m1[s[i]]=t[i];
                 m2[t[i]]=s[i];

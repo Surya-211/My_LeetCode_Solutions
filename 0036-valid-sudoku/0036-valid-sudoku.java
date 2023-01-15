81373f3ac4f3e9389e3c064ee27709e58290c02d
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-01-16 01:00:31 +0530

    Time: 16 ms (28.54%), Space: 43.1 MB (68.80%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,20 @@
 class Solution {
-    public:
-bool f(vector<vector<char>>&board,char c,int i,int j){
-        for(int k=0;k<9;k++){
-            if(k==j) continue;
-            if(board[i][k]==c) return false;
-        }
-         for(int k=0;k<9;k++){
-            if(k==i) continue;
-            if(board[k][j]==c) return false;
-        }  
-        int x=i/3*3;
-        int y=j/3*3;
-        for(int u=x;u<x+3;u++){
-            for(int v=y;v<y+3;v++){
-                if(u==i && v==j) continue;
-                if(board[u][v]==c) return false;
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> s=new HashSet<String>();
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    String num="";
+                    num+=board[i][j];
+                    int b=(i/3)*3+j/3;
+                    if(!s.add("row"+i+num)||!s.add("col"+j+num)||!s.add("box"+b+num))
+                        return false;
+                }
             }
         }
         return true;
     }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.') continue;
-                if(!f(board,board[i][j],i,j)) return false;
-            }
-        }
-        return true;
-    }
-};
+}

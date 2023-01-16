2df76f0e2b3928b5c5656f2ce8a0d34d212ae02e
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-01-16 21:26:15 +0530

    Time: 3 ms (36.04%), Space: 6 MB (69.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,15 +7,15 @@ public:
             return 1;
         if(n==2)
             return 2;
-        int two=1;
-        int one=2;
+        int one=1;
+        int two=2;
         int total=0;
         int i=2;
         while(i<n)
         {
             total=one+two;
-            two=one;
-            one=total;
+            one=two;
+            two=total;
             i++;
         }
         return total;

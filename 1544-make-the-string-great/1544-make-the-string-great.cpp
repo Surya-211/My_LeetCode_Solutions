2fac3eaf982bae25e96a2a07fba33773576d7fc9
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-01-28 00:57:42 +0530

    Time: 3 ms (69.69%), Space: 7 MB (18.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
-    string makeGood(string s) {
-         // if s has less than 2 characters, we just return itself.
-        while (s.size() > 1) {
-            // 'find' records if we find any pair to remove.
-            bool find = false;
-            
-            // Check every two adjacent characters, currChar and nextChar.
-            for (int i = 0; i < s.size() - 1; ++i) {
-                char currChar = s[i], nextChar = s[i + 1];
-                
-                // If they make a pair, remove them from 's' and let 'find = true'.
-                if (abs(currChar - nextChar) == 32) {
-                    s = s.substr(0, i) + s.substr(i + 2);
-                    find = true;
-                    break;
+    string makeGood(string str) {
+        stack<char>s;
+        for(int i=0;i<str.size();i++)
+        {
+            if(s.empty())
+            {
+                s.push(str[i]);
+                continue;
+            }
+            char ch=s.top();
+            if(islower(str[i]))
+            {
+                if(ch==toupper(str[i]))
+                {
+                    s.pop();
+                    continue;
                 }
+                else
+                    s.push(str[i]);
+            }
+            else
+            {
+                if(ch==tolower(str[i]))
+                {
+                    s.pop();
+                    continue;
+                }
+                else
+                    s.push(str[i]);
             }
             
-            // If we cannot find any pair to remove, break the loop. 
-            if (!find)
-                break;
         }
-        return s;
+        string st="";
+        while(!s.empty())
+        {
+            st=s.top()+st;
+            s.pop();
+        }
+        return st;
     }
 };

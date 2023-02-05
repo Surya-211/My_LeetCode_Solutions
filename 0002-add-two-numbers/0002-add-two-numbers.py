a2029e0be707bcc9ee4fbb7c29ef329ebe3e27ad
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-05 19:29:52 +0700

    add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,75 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
-        ListNode* ans=NULL;
-        int carry=0;
-        ListNode* prev=NULL;
-        while(l1!=NULL&&l2!=NULL)
-        {
-            int sum=l1->val+l2->val+carry;
-            carry=sum/10;
-            int num=sum%10;
-            ListNode* temp=new ListNode();
-            temp->val=num;
-            temp->next=NULL;
-            if(ans==NULL)
-            {
-                prev=temp;
-                ans=temp;
-            }
-            else
-            {
-                prev->next=temp;
-                prev=temp;
-            }
-            l1=l1->next;
-            l2=l2->next;
-        }
-        while(l1!=NULL)
-        {
-            ListNode* temp=new ListNode();
-            int sum=l1->val+carry;
-            carry=sum/10;
-            int num=sum%10;
-            temp->val=num;
-            temp->next=NULL;
-            prev->next=temp;
-            prev=temp;
-            l1=l1->next;
-        }
-         while(l2!=NULL)
-        {
-            ListNode* temp=new ListNode();
-            int sum=l2->val+carry;
-            carry=sum/10;
-            int num=sum%10;
-            temp->val=num;
-            temp->next=NULL;
-            prev->next=temp;
-            prev=temp;
-            l2=l2->next;
-        }
-        if(carry==1)
-        {
-            ListNode* temp=new ListNode();
-            temp->val=carry;
-            temp->next=NULL;
-            prev->next=temp;
-        }
-        return ans;
-    }
-};
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            sum = carry
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+                
+            carry = sum // 10
+            sum = sum % 10
+            current.next = ListNode(sum)
+            current = current.next
+            
+        return dummy.next

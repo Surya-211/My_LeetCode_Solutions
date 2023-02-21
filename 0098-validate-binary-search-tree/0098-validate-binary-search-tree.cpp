b9187dac377ff8670ff49874fee5eaf913dc1d0f
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-02-21 22:05:50 +0530

    Time: 19 ms (27.64%), Space: 21.7 MB (55.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -12,27 +12,33 @@
 class Solution {
 public:
     int flag=1;
-    long min=LONG_MIN;
+    long mini=LONG_MIN;
     bool isValidBST(TreeNode* root) {
-       if(root==NULL)
-           return true;
+        
+        if(root==NULL)
+            return true;
         inorder(root);
         if(flag==1)
             return true;
         else
             return false;
+        
     }
+    
     void inorder(TreeNode* root)
     {
         if(root==NULL)
             return;
         inorder(root->left);
-        if(root->val<=min)
+        if(root->val>mini)
+        {
+            mini=root->val;
+        }
+        else
         {
             flag=0;
             return;
         }
-        min=root->val;
         inorder(root->right);
     }
 };

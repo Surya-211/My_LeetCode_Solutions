dd596f47c4727b8cb8dffde59637693c14808867
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-02-21 11:08:14 +0530

    Time: 30 ms (55.07%), Space: 22.5 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -8,23 +8,10 @@ public:
         {
             int mid=(low+high)/2;
             
-            if(mid<nums.size()-1 && mid>0 && nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
-                return nums[mid];
-            
-            if(mid%2==0)
-            {
-                if(mid<nums.size()-1 && nums[mid]==nums[mid+1])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]))
+                low=mid+1;
             else
-            {
-                if(mid>0 && nums[mid]==nums[mid-1])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }
+                high=mid;
         }
         return nums[low];
         

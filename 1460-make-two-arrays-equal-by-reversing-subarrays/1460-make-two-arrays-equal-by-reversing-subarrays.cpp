20237e0217cc842241a0c59cd7ca4db42dfd6aec
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-03-29 23:21:28 +0530

    Time: 14 ms (62.42%), Space: 14.4 MB (70.05%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        
+        if(target.size()!=arr.size())
+            return false;
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++)
+        {
+            if(target[i]!=arr[i])
+                return false;
+        }
+        return true;
+    }
+};

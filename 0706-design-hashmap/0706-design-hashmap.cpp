d84e543b0c186e578094694b6f639fdfe78fefd6
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-04-14 03:03:09 +0530

    Time: 125 ms (80.69%), Space: 52.4 MB (74.72%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+public:
+    
+    unordered_map<int,int>m;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    int get(int key) {
+        if(m.find(key)!=m.end())
+            return m[key];
+        else
+            return -1;
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */

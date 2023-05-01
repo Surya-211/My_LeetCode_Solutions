63cdefae66c84c9af3f71dba3e28ac1864e914ff
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-05-01 21:11:49 +0530

    Time: 16 ms (51.34%), Space: 9.5 MB (56.71%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -6,19 +6,12 @@ public:
         int s=target.size();
         for(int i=s-1;i>=0;i--)
         {
-            if(i==s-1 && target[i]=='1')
+            if(target[i]=='1')
             {
-                count++;
-                i--;
-                while(i>=0 && target[i]=='1')
-                    i--;
-                i++;
-            }
-            else if(i==0 && target[i]=='1')
-                count+=2;
-            else if(target[i]=='1')
-            {
-                count+=2;
+                if(i==s-1)
+                    count++;
+                else
+                    count+=2;
                 i--;
                 while(i>=0 && target[i]=='1')
                     i--;

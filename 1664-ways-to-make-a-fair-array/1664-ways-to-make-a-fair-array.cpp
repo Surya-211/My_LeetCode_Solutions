c705750d08b1b6a10398ca3bfcb6df9de30a7946
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-05-30 18:45:41 +0530

    Time: 513 ms (5.05%), Space: 160.2 MB (5.05%) - LeetHub

---
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int sum_even=0;
+        int sum_odd=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i%2==0)
+            {
+                sum_even+=nums[i];
+                m[i]=sum_even;
+            }
+            else
+            {
+                sum_odd+=nums[i];
+                m[i]=sum_odd;
+            }
+        }
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i==0)
+            {
+                int s1=sum_even-nums[i];
+                int s2=sum_odd;
+                if(s1-s2==0)
+                    count++;
+            }
+            else if(i==nums.size()-1)
+            {
+                if(i%2==0)
+                {
+                    int s1=sum_even-nums[i];
+                    int s2=sum_odd;
+                    if(s1-s2==0)
+                        count++;
+                }
+                else
+                {
+                    int s1=sum_even;
+                    int s2=sum_odd-nums[i];
+                    if(s1-s2==0)
+                        count++;
+                }
+            }
+            else
+            {
+                if(i%2==0)
+                {
+                    int s=sum_even-nums[i];
+                    int s1=m[i]-nums[i];
+                    s1+=sum_odd-m[i-1];
+                    int s2=m[i-1];
+                    s2+=sum_even-m[i];
+                    if(s1-s2==0)
+                        count++;
+                }
+                else
+                {
+                    int s=sum_odd-nums[i];
+                    int s1=m[i]-nums[i];
+                    s1+=sum_even-m[i-1];
+                    int s2=m[i-1];
+                    s2+=sum_odd-m[i];
+                    if(s1-s2==0)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+};

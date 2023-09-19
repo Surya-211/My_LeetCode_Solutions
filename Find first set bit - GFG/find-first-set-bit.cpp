000a683db6aeced287e34169acd3d197abdde527
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-09-19 17:43:50 +0530

    Added solution - LeetHub

---
+++ Find first set bit - GFG/find-first-set-bit.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        // Your code here
+        vector<int>v;
+        while(n>0)
+        {
+            v.push_back(n%2);
+            n/=2;
+        }
+        int ans=1;
+        reverse(v.begin(),v.end());
+        for(int i=v.size()-1;i>=0;i--)
+        {
+            if(v[i]==1)
+            {
+                return ans;
+            }
+            ans++;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends

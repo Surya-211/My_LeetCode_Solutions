b9978fe2c277945e53bde0ec12390b53bb82b460
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-12-16 23:16:29 +0530

    Time: 10 ms (52.26%), Space: 7.7 MB (79.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for(auto i:m)
+        {
+            if(i.second!=0)
+                return false;
+        }
+        return true;
+    }
+};

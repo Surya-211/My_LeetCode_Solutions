af0d1d0f54ef5f7fddf2e016ddc34c00fce2d068
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-12-19 21:39:48 +0530

    Time: 47 ms (34.77%), Space: 22 MB (59.38%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size();
+        int n = img[0].size();
+        vector<vector<int>> v(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (0 <= x && x < m && 0 <= y && y < n) {
+                            sum += img[x][y];
+                            count += 1;
+                        }
+                    }
+                }
+                v[i][j] = sum / count;
+            }
+        }
+        return v;
+    }
+};

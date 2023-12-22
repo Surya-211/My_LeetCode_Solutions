e0bf44b17097df9cde395076a9e29ea1eb78d1a5
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2023-12-23 00:34:57 +0530

    Time: 226 ms (5.04%), Space: 81.3 MB (5.42%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int left=0;
-        int right=nums.size()-1;
-        int count=0;
-        while(left<right)
+        // sort(nums.begin(),nums.end());
+        // int left=0;
+        // int right=nums.size()-1;
+        // int count=0;
+        // while(left<right)
+        // {
+        //     int sum=nums[left]+nums[right];
+        //     if(sum==k)
+        //     {
+        //         count++;
+        //         left++;
+        //         right--;
+        //     }
+        //     else if(sum<k)
+        //         left++;
+        //     else
+        //         right--;
+        // }
+        // return count;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
         {
-            int sum=nums[left]+nums[right];
-            if(sum==k)
+            m[nums[i]]++;
+        }
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m[nums[i]]==0)
+                continue;
+            int x=k-nums[i];
+            if(x==nums[i] && m[x]==1)
+                continue;
+            if(m.find(x)!=m.end() &&m[x]>0)
             {
                 count++;
-                left++;
-                right--;
+                m[x]--;
+                m[nums[i]]--;
             }
-            else if(sum<k)
-                left++;
-            else
-                right--;
+            if(m[nums[i]]<=0)
+                m.erase(nums[i]);
+            if(m[x]<=0)
+                m.erase(x);
         }
         return count;
     }

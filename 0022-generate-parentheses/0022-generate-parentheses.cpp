39abb78a2123a2e5a37fd0e9b5edf152b0afa454
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2024-04-09 01:35:02 +0530

    Time: 3 ms (60.73%), Space: 11.8 MB (81.14%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void func(int n,int i,int open,int close,string &s,vector<string>&ans)
+    {
+        if(i==(2*n))
+        {
+            ans.push_back(s);
+            return;
+        }
+        if(s.empty() || open==close)
+        {
+            s.push_back('(');
+            func(n,i+1,open+1,close,s,ans);
+            s.pop_back();
+        }  
+        else
+        {
+            if(open<n)
+            {
+                s.push_back('(');
+                func(n,i+1,open+1,close,s,ans);
+                s.pop_back();
+            }
+            s.push_back(')');
+            func(n,i+1,open,close+1,s,ans);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string s="";
+        int i=0;
+        int open=0;
+        int close=0;
+        func(n,i,open,close,s,ans);
+        return ans;
+    }
+};

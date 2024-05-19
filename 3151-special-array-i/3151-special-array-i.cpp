ce169ed6493e2b5195c839896f9973468516268e
Surya Prakash <85299721+Surya-211@users.noreply.github.com>
2024-05-19 22:23:28 +0530

    Time: 4 ms (80.00%), Space: 26.8 MB (60.00%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]%2==0 && nums[i-1]%2==0)
+                return false;
+            else if(nums[i]%2!=0 && nums[i-1]%2!=0)
+                return false;
+        }
+        return true;
+    }
+};
